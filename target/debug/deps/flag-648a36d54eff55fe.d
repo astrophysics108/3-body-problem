8fad9ea72e843343d135b98693f9e30f31510fe0
Delana <dsingh@perse.co.uk>
2025-11-14 12:09:06 +0000

    Stop tracking target/ and ignore build artifacts

--- target/debug/deps/flag-648a36d54eff55fe.d
+++
@@ -1,5 +0,0 @@
-/workspaces/3-body-problem/three-body-problem/target/debug/deps/flag-648a36d54eff55fe.d: src/main.rs
-
-/workspaces/3-body-problem/three-body-problem/target/debug/deps/flag-648a36d54eff55fe: src/main.rs
-
-src/main.rs:
--- target/debug/flag.d
+++
@@ -1,1 +0,0 @@
-/workspaces/3-body-problem/target/debug/flag: /workspaces/3-body-problem/src/main.rs
